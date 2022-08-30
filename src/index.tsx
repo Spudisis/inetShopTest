0dc24bb1e0204ce48701fcd18fb320a0aa6729bb
Spudisis <alekasisbb@gmail.com>
2022-08-30 23:48:21 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+import { HashRouter } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
